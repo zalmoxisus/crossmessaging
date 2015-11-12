097b8bcda43f9f0c2b3054b3c60c3ed96982efd1
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-12 15:31:42 +0200

    v0.1.0

---
+++ src/index.js
@@ -0,0 +1,38 @@
+export function onConnect(respond, connections = {}, tab, error) {
+  chrome.runtime.onConnect.addListener(function(port) {
+    function extensionListener(message) {
+      if (message.name === 'init') {
+        connections[message.tabId] = port;
+
+        if (tab && message.tabId !== tab.id) {
+          error(port);
+          return;
+        }
+        connections[message.tabId].postMessage(respond());
+      }
+    }
+
+    port.onMessage.addListener(extensionListener);
+    port.onDisconnect.addListener(function(portDiscon) {
+      portDiscon.onMessage.removeListener(extensionListener);
+      Object.keys(connections).forEach(function(id) {
+        if (connections[id] === portDiscon) {
+          delete connections[id];
+        }
+      });
+    });
+
+  });
+}
+
+export const connect = chrome.runtime.connect;
+
+export function onMessage(messaging) {
+  if (chrome.runtime.onMessage) chrome.runtime.onMessage.addListener(messaging);
+}
+
+export const sendToBg = chrome.runtime.sendMessage;
+
+export function sendToTab(...args) {
+  chrome.tabs.sendMessage(...args);
+}
